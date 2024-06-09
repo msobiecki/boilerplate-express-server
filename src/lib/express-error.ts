0e50c63d560fa7fc5d688d804c4f49cc8a468884
Michał Sobiecki <michal.sobiecki@pracuj.pl>
2024-06-09 17:32:40 +0200

    feat: clean

--- src/lib/express-error.ts
+++
@@ -1,7 +0,0 @@
-import { taggedLogger } from "./logger";
-
-const logger = taggedLogger("server");
-
-const expressError = (error: Error) => logger.error("Server is crashed", error);
-
-export default expressError;
